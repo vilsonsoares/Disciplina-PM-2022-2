7cdd6576942df049fcc1682cecbf9b8090b810e7
Vilson Soares <vilsonsoares@gmail.com>
2022-07-28 12:07:37 -0300

    Teste do Git 3

--- teste.dart
+++ teste.dart
@@ -1,9 +1,13 @@
 void main() {
   var pessoa = Nome();
-  print("Olá: "+pessoa.nome + ' ' + pessoa.sobrenome);
+  print("Olá: "+ pessoa.pessoa());
 }
 
 class Nome {
   var nome = "Vilson";
   var sobrenome = "Soares de Siqueira";
+
+  pessoa() {
+    return this.nome + " " + this.sobrenome;
+  }
 }
